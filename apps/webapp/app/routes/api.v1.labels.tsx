ce43bf6a4348a4587caad3a46891aa5e7b111f41
Harshith Mullapudi <harshithmullapudi@gmail.com>
2025-11-30 10:17:29 +0530

    feat: add /api/v1/labels api

---
+++ apps/webapp/app/routes/api.v1.labels.tsx
@@ -0,0 +1,24 @@
+import { json } from "@remix-run/node";
+import { createHybridLoaderApiRoute } from "~/services/routeBuilders/apiBuilder.server";
+
+import { LabelService } from "~/services/label.server";
+import { getWorkspaceByUser } from "~/models/workspace.server";
+
+// This route handles the OAuth redirect URL generation, similar to the NestJS controller
+const loader = createHybridLoaderApiRoute(
+  {
+    allowJWT: true,
+    findResource: async () => 1,
+    corsStrategy: "all",
+  },
+  async ({ authentication }) => {
+    const labelService = new LabelService();
+    const workspace = await getWorkspaceByUser(authentication.userId);
+
+    const labels = labelService.getWorkspaceLabels(workspace?.id as string);
+
+    return json(labels);
+  },
+);
+
+export { loader };

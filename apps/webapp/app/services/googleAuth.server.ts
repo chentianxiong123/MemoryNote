081ec6ac861513e7229d0bf39dafa117252174db
Harshith Mullapudi <harshithmullapudi@gmail.com>
2026-01-02 23:59:52 +0530

    fix: add google scopes to authentication

--- apps/webapp/app/services/googleAuth.server.ts
+++ apps/webapp/app/services/googleAuth.server.ts
@@ -18,7 +18,8 @@ export function addGoogleStrategy(
       scopes: [
         "https://www.googleapis.com/auth/gmail.send",
         "https://www.googleapis.com/auth/gmail.modify",
-        "https://www.googleapis.com/auth/gmail.labels",
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile",
         "https://www.googleapis.com/auth/calendar",
       ],
       redirectURI: `${env.LOGIN_ORIGIN}/auth/google/callback`,
